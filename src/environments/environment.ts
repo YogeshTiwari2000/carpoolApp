a3f074252ae8089b7b5a228596f7ff81500a3aac
YogeshTiwari2000 <yogeshtiwari.b.007@gmail.com>
2025-03-13 16:22:33 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyALZM4O-UmFJ2SMqFTtN9heqkqh6fsKZGM",
+    authDomain: "carhai-24bb2.firebaseapp.com",
+    projectId: "carhai-24bb2",
+    storageBucket: "carhai-24bb2.firebasestorage.app",
+    messagingSenderId: "1072526572389",
+    appId: "1:1072526572389:web:5fa556899274a04ef1ff8c",
+    measurementId: "G-M7S2Z59QH0"
+  }
+};
+
